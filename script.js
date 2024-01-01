0b684ef86f8e340fe3f29f07c20cc7f3a30bf828
Adil Wadud <adilwadud10@gmail.com>
2024-01-01 15:02:30 +0000

    updated everything

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ function scrollAbout() {
     const about = document.querySelector(".about");
     about.scrollIntoView();
 }
+
+
+function scrollTechStack() {
+    const techStack = document.querySelector(".tech-stack");
+    techStack.scrollIntoView();
+}
